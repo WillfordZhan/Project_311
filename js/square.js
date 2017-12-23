ef1770e373cc89b2cddcd60e897792c62da5c47b
WillfordZhan <willfordzhan@gmail.com>
2017-12-23 13:11:11 +0800

    failed to implement the card effect, begin another part

--- js/square.js
+++ js/square.js
@@ -1,5 +1,13 @@
+jQuery(document).ready(function($){
+    var img = $('.unselected');
+    img.hover(
+        function () {
+            img.addClass("selected"); 
+    }
+);
 $(".selected").hover(function(){
     $(this).stop(true).animate({left:0},500);
 },function(){
     $(this).stop(true).animate({left:-100},500);
+});
 });
