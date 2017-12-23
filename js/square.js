dcb9c9da5f39efb4ab52991324563f68c17e066c
WillfordZhan <willfordzhan@gmail.com>
2017-12-23 23:29:57 +0800

    1. added acitvity info and club name beneth the card 2. made the activity info and name animation(hide and show) 3. optimized the tranverse effect, need to be more(by animation)

--- js/square.js
+++ js/square.js
@@ -1,13 +1,36 @@
 jQuery(document).ready(function($){
-    var img = $('.unselected');
-    img.hover(
-        function () {
-            img.addClass("selected"); 
-    }
-);
-$(".selected").hover(function(){
+    var ismouseenter = false // 初态未移入鼠标
+    var imgdiv = $('.unselected');
+    var img = $('img-card');
+    var hidden = $('.hidden-info');
+    imgdiv.mouseenter(function(){
+        //alert('');
+        if (ismouseenter == true) {
+            return;
+        }
+        hidden.animate({
+            opacity:1
+        },500);
+        ismouseenter = true;
+    });
+    imgdiv.mouseleave(function (){
+        //alert('');
+        if (ismouseenter == false) {
+            return;
+        }
+        hidden.animate({
+            opacity:0
+        },500);
+        ismouseenter = false;
+    });
+});
+
+
+
+/*$(".selected").hover(function(){
     $(this).stop(true).animate({left:0},500);
 },function(){
     $(this).stop(true).animate({left:-100},500);
 });
 });
+*/
