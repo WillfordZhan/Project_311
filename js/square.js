4485440aaf315fc19bc97d635b97dcf6d2987112
WillfordZhan <willfordzhan@gmail.com>
2017-12-24 19:55:48 +0800

    !!!! finished card effect!!!

--- js/square.js
+++ js/square.js
@@ -1,27 +1,33 @@
 jQuery(document).ready(function($){
     var ismouseenter = false // 初态未移入鼠标
     var imgdiv = $('.unselected');
-    var img = $('img-card');
-    var cardContent = $('.card-content');
-    imgdiv.mouseenter(function(){
-        //alert('');
-        if (ismouseenter == true) {
-            return;
-        }
-        cardContent.animate({
-            opacity:1
-        },500);
-        ismouseenter = true;
-    });
-    imgdiv.mouseleave(function (){
-        //alert('');
-        if (ismouseenter == false) {
-            return;
-        }
-        cardContent.animate({
-            opacity:0
-        },500);
-        ismouseenter = false;
+    imgdiv.each(function(){
+        var img = $(this).find(('.img-card'));
+        var cardContent = $(this).find(('.card-content'));
+        $(this).mouseenter(function(){
+            //alert('');
+            if (ismouseenter == true) {
+                return;
+            }
+            cardContent.animate({
+                opacity:1
+            },500);
+            //alert('');
+            img.addClass("img-card-move");
+            //alert('');
+            ismouseenter = true;
+        });
+        $(this).mouseleave(function (){
+            //alert('');
+            if (ismouseenter == false) {
+                return;
+            }
+            cardContent.animate({
+                opacity:0
+            },500);
+            img.removeClass("img-card-move");
+            ismouseenter = false;
+        });
     });
 });
 
